5d8206c3fb85f1314f19ea45968811e4ec2a009c
Manuel Rojas <m.rojas@unas.edu.pe>
2020-10-12 16:27:25 -0500

    aa

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const server = express();
+const morgan=require('morgan')
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const { request, response } = require("express");
+const dbName = "curso";
+const collectionName = "cursos";
+
+
+db.initialize(dbName, collectionName, function (dbCollection) { // successCallback
+   server.get("/ListCourse", (request, response) => {
+      // return updated list
+      dbCollection.find().toArray((error, result) => {
+         if (error) throw error;
+         response.json(result);
+      });
+   });
+   
+   server.get ("/",(request,response)=>{
+      response.send("hello world ")
+   })
+}, function (err) { 
+   throw (err);
+});
+
+server.listen(port, () => {
+   console.log(`Server listening at ${port}`);
+});
